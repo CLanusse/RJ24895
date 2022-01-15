e920761ee476a9093b4fa91f4c29479b122dc3c5
Conrado Lanusse <conrado.lanusse@gmail.com>
2022-01-15 14:30:34 -0300

    Primera clase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Seccion from './Seccion.js'
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hola mundo!</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <Seccion/>
+      <Seccion/>
+      <Seccion/>
+      <Seccion/>
     </div>
   );
 }
---
+++ src/Seccion.js
@@ -0,0 +1,18 @@
+
+
+
+const Seccion = () => {
+
+    return (
+        <section>
+            <h2>Contenido adicional</h2>
+            <ul>
+                <li>Hola mundo 1</li>
+                <li>Hola mundo 2</li>
+                <li>Hola mundo 3</li>
+            </ul>
+        </section>
+    )
+}
+
+export default Seccion
